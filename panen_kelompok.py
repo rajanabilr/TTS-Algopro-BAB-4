ad1674d9b33c682a9302456c88649670fa3f75eb
estervinia <viniaester@apps.ipb.ac.id>
2026-09-17 20:31:29 +0700

    Menambahkan fungsi laporan hasil panen

--- panen_kelompok.py
+++ panen_kelompok.py
@@ -3,3 +3,9 @@
 data_panen = []
 
 print("Sistem Pencatatan Hasil Panen Digital")
+def tampilkan_laporan():
+    total = sum(data_panen)
+
+    print("=== LAPORAN HASIL PANEN ===")
+    print("Data panen:", data_panen)
+    print("Total hasil panen:", total, "kg")
