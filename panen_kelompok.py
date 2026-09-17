6d4e2a69b2a0446635ace5273b648fce8722cc5f
rajanabilr <rajanabilrafliansyah@gmail.com>
2026-09-17 22:04:44 +0700

    Mengubah judul oleh Anggota B

--- panen_kelompok.py
+++ panen_kelompok.py
@@ -8,7 +8,6 @@ def tambah_data_panen(jumlah):
     print("Data panen berhasil ditambahkan:", jumlah, "kg")
 
 
-
 print("Sistem Pencatatan Hasil Panen Digital - Anggota B")
 def tampilkan_laporan():
     total = sum(data_panen)
